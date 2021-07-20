1bf001805d3197fd3bdeb255cb0ceaf83a5245e3
Esteban Borai <estebanborai@gmail.com>
2021-07-19 20:47:21 -0400

    chore: run "cargo fmt" to lint codebase

--- src/lib/cine.rs
+++ src/lib/cine.rs
@@ -1,17 +1,16 @@
+use super::Asiento;
+use super::Sala;
+use crate::utils::read_line_and_parse;
 use std::io;
 use std::str::FromStr;
-use super::Sala;
-use super::Asiento;
-use crate::utils::read_line_and_parse;
 
-#[derive(Debug,Clone)]
+#[derive(Debug, Clone)]
 pub struct Cine {
     pub nombre: String,
     pub salas: Vec<Sala>,
 }
 
-
-impl FromStr for Cine{
+impl FromStr for Cine {
     type Err = ();
     fn from_str(nombre: &str) -> Result<Self, Self::Err> {
         Ok(Cine::new(nombre.trim()))
@@ -33,10 +32,13 @@ impl Cine {
 
     pub fn to_string(&self, sala: usize) -> String {
         let mut s = String::new();
-        for i in 0..self.salas[sala].asientos.capacity(){
-            for j in 0..self.salas[sala].asientos[i].capacity(){
-                if self.salas[sala].asientos[i][j].ocupado { s.push_str("1"); }
-                else { s.push_str("0"); }
+        for i in 0..self.salas[sala].asientos.capacity() {
+            for j in 0..self.salas[sala].asientos[i].capacity() {
+                if self.salas[sala].asientos[i][j].ocupado {
+                    s.push_str("1");
+                } else {
+                    s.push_str("0");
+                }
             }
         }
 
@@ -49,13 +51,12 @@ impl Cine {
         s.push_str("Sala");
         s.push_str("\n");
         let mut cantidad_de_ocupados = 0;
-        for i in 0..self.salas[sala].asientos.capacity(){
-            for j in 0..self.salas[sala].asientos[i].capacity(){
+        for i in 0..self.salas[sala].asientos.capacity() {
+            for j in 0..self.salas[sala].asientos[i].capacity() {
                 if self.salas[sala].asientos[i][j].ocupado {
                     cantidad_de_ocupados = cantidad_de_ocupados + 1;
-                }
-                else{ 
-                    cantidad_de_ocupados = cantidad_de_ocupados + 0; 
+                } else {
+                    cantidad_de_ocupados = cantidad_de_ocupados + 0;
                 }
             }
         }
@@ -64,13 +65,12 @@ impl Cine {
         s
     }
 
-
     pub fn comprar_entrada(&mut self) {
-        let mut sala : usize = 0;
+        let mut sala: usize = 0;
         while sala <= 0 || sala > 6 {
             println!("Seleccione la sala a elegir:");
             sala = read_line_and_parse().unwrap();
-            if sala <= 0 || sala > 6{
+            if sala <= 0 || sala > 6 {
                 println!("Sala inválida");
                 println!("Solo números entre 1 y 5");
             }
@@ -80,39 +80,34 @@ impl Cine {
         while cantidad <= 0 || cantidad > 501 {
             println!("Ingrese la cantidad de entradas a comprar:");
             cantidad = read_line_and_parse().unwrap();
-            if cantidad <= 0 || cantidad > 501{
+            if cantidad <= 0 || cantidad > 501 {
                 println!("Cantidad inválida");
                 println!("Solo números entre 1 y 501");
             }
         }
 
-
-        for _ in 0..cantidad{
+        for _ in 0..cantidad {
             let mut fila: usize = 0;
             while fila <= 0 || fila > 11 {
                 println!("Seleccione la fila del asiento a ocupar");
                 fila = read_line_and_parse().unwrap();
-                if fila <= 0 || fila > 11{
+                if fila <= 0 || fila > 11 {
                     println!("Fila inválida");
                     println!("Solo números entre 1 y 10");
                 }
             }
 
-
             let mut columna: usize = 0;
             while columna <= 0 || columna > 51 {
                 println!("Seleccione la columna del asiento a ocupar");
                 columna = read_line_and_parse().unwrap();
-                if columna <= 0 || columna > 51{
+                if columna <= 0 || columna > 51 {
                     println!("Fila inválida");
                     println!("Solo números entre 1 y 51");
                 }
             }
 
-
-            
-            self.salas[sala-1].asientos[fila-1][columna-1].ocupado = true;
-            
+            self.salas[sala - 1].asientos[fila - 1][columna - 1].ocupado = true;
         }
     }
 }
--- src/utils/stdin.rs
+++ src/utils/stdin.rs
@@ -1,6 +1,6 @@
-use std::{io, str::FromStr};
-use num::Integer;
 use num::Float;
+use num::Integer;
+use std::{io, str::FromStr};
 
 pub fn read_line() -> String {
     let mut s: String = String::new();
@@ -11,4 +11,3 @@ pub fn read_line() -> String {
 pub fn read_line_and_parse<T: FromStr>() -> Result<T, <T as FromStr>::Err> {
     Ok(T::from_str(read_line().trim())?)
 }
-
